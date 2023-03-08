9dfe54f8555c55493b3689e202cba9e668ebd52b
larissabileski <laricristinabileski@gmail.com>
2023-03-08 11:46:42 -0300

    Input

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,29 @@
 import { Component } from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import { Button, Text, View, StyleSheet, TextInput } from 'react-native';
 
 export default class Counter extends Component {
     state = {
         count: 0,
+        step: 2,
     };
     incrementar(){
-        this.setState({ count: this.state.count + 1});
+        this.setState({ count: this.state.count + this.state.step});
     }
     decrementar(){
         this.setState({ count: this.state.count - 1});
     }
+    alteraStep(text){
+        this.setState({ step: parseInt(text)});
+    }
     render() {
         return (
             <View style={StyleSheet.contador}>
+            <TextInput
+                defaultValue={this.state.step} 
+                onChangeText={(text) => this.alteraStep(text)}
+                inputMode="numeric"
+                keyboardType="numeric"
+            />
             <Text>Contador: {this.state.count}</Text>
             <Button onPress={() => this.incrementar()} title='+'/>
             <Button onPress={() => this.decrementar()} title='-'/>
